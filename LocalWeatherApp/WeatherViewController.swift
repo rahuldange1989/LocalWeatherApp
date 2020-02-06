1d4f75a36191446e1a088811b11869095aec6b2b
Rahul <rahul.dange1989@gmail.com>
2020-02-06 21:33:58 +0100

    Created basic project.

---
+++ LocalWeatherApp/WeatherViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherViewController.swift
+//  LocalWeatherApp
+//
+//  Created by Rahul Dange on 06/02/20.
+//  Copyright © 2020 Rahul Dange. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
